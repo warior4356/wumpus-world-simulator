064a839aaacc6cd3470d3ea7ec7be63f0c5b52f0
warior4356 <warior4356@yahoo.com>
2021-10-06 14:29:47 -0700

    HW2 Done

--- Agent.py
+++ Agent.py
@@ -2,40 +2,93 @@
 
 import sys
 import Action
+import random
 
 class Agent:
+    # Updates facing after a left turn
+    def turn_left(self):
+        left_turn = {
+            "E": "N",
+            "N": "W",
+            "W": "S",
+            "S": "E"
+        }
+        self.facing = left_turn[self.facing]
+
+    # Updates facing after a right turn
+    def turn_right(self):
+        right_turn = {
+            "E": "S",
+            "S": "W",
+            "W": "N",
+            "N": "E"
+        }
+        self.facing = right_turn[self.facing]
+
+    # Updates position after a move
+    def record_move(self):
+        if self.facing == "E":
+            self.location_x = self.location_x + 1
+        elif self.facing == "W":
+            self.location_x = self.location_x - 1
+        elif self.facing == "N":
+            self.location_y = self.location_y + 1
+        elif self.facing == "S":
+            self.location_y = self.location_y - 1
+
     def __init__(self):
-        pass
+        self.debug = True
     
     def __del__(self):
         pass
     
     def Initialize(self):
-        pass
+        self.facing = "E"
+        self.location_x = 1
+        self.location_y = 1
+        self.moved = False
+        self.gold = False
+        self.arrow = True
     
     def Process(self, percept):
-        valid_action = False
-        while not valid_action:
-            valid_action = True
-            if sys.version_info[0] < 3:
-                c = raw_input("Action? ") # Python 2
-            else:
-                c = input("Action? ") # Python 3
-            if c == 'f':
+        # Record successful movements
+        if self.moved and not percept.bump:
+            self.record_move()
+        self.moved = False
+        if self.debug:
+            print("X = " + str(self.location_x) + " Y = " + str(self.location_y))
+            print("facing = " + str(self.facing))
+            input("Next? ")
+
+        # If there is gold, grab it
+        if percept.glitter:
+            action = Action.GRAB
+            self.gold = True
+        # If the agent has the gold on (1,1), leave
+        elif self.location_x == 1 and self.location_y == 1 and self.gold:
+            action = Action.CLIMB
+        # If the agent smells the wumpus, shoot
+        elif percept.stench and self.arrow:
+            action = Action.SHOOT
+            self.arrow = False
+        # Random movement
+        else:
+            # Pick a random number between 0 and 2 (inclusive)
+            think = random.randint(0, 2)
+            if self.debug:
+                print("Think = " + str(think))
+            # If the number is 0, go forward
+            if think == 0:
                 action = Action.GOFORWARD
-            elif c == 'l':
+                self.moved = True
+            # If the number is 1, turn left
+            elif think == 1:
                 action = Action.TURNLEFT
-            elif c == 'r':
+                self.turn_left()
+            # If the number is 2, turn right
+            elif think == 2:
                 action = Action.TURNRIGHT
-            elif c == 'g':
-                action = Action.GRAB
-            elif c == 's':
-                action = Action.SHOOT
-            elif c == 'c':
-                action = Action.CLIMB
-            else:
-                print("Huh?")
-                valid_action = False
+                self.turn_right()
         return action
     
     def GameOver(self, score):
